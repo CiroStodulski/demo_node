989e1b9dbfcd6793f30ce2494e59ebc2b361d706
Ciro Stodulski de Azevedo <ciro.sda@gmail.com>
2018-03-14 10:56:16 -0300

    settings

--- server/express.js
+++ server/express.js
@@ -1,7 +1,6 @@
 const express = require('express'),
     consign = require('consign'),
     bodyParser = require('body-parser'),
-    multparty = require('connect-multiparty'),
     app = express();
 
 app.set('view engine', 'ejs');
@@ -9,7 +8,6 @@ app.set('views', './app/views')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(multparty());
 
 app.use(express.static('./app/public'));
 
@@ -20,5 +18,4 @@ consign()
     .then('app/routes')
     .into(app)
     
-
 module.exports = app;
